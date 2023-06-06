7f9f4180007bafb2426b9eddbeb846c0a83f5881
Emmanuel <manuboisset@gmail.com>
2023-06-06 20:05:42 +0200

    Update to service

--- RentPropertiesAPI/src/main/java/fr/rent/application/RentPropertyController.java
+++ RentPropertiesAPI/src/main/java/fr/rent/application/RentPropertyController.java
@@ -5,6 +5,8 @@ import fr.rent.dto.RentPropertyResponseDto;
 import fr.rent.exception.RentPropertyNotFoundException;
 import fr.rent.mapper.RentPropertyDtoMapper;
 import fr.rent.repository.RentPropertyRepository;
+import fr.rent.service.RentPropertyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,17 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/rent-properties-api")
 public class RentPropertyController {
 
-    private final RentPropertyRepository rentalPropertyRepository;
+    private final RentPropertyService rentalPropertyRepository;
     private final RentPropertyDtoMapper rentalPropertyDtoMapper;
-
-    public RentPropertyController(RentPropertyRepository rentalPropertyRepository,
-                                  RentPropertyDtoMapper rentalPropertyDtoMapper) {
-        this.rentalPropertyRepository = rentalPropertyRepository;
-        this.rentalPropertyDtoMapper = rentalPropertyDtoMapper;
-    }
 
 
     @GetMapping("/rental-properties")
