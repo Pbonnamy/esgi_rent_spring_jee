6f44f7f4fdcd391d1cd5a944f9706ec8e3f6fc33
noeoxycode <76428537+noeoxycode@users.noreply.github.com>
2023-06-07 02:08:36 +0200

    first endpoint done : get rental properties

--- RentFrontAPI/src/main/java/fr/esgi/rent/api/RentalPropertyResource.java
+++ RentFrontAPI/src/main/java/fr/esgi/rent/api/RentalPropertyResource.java
@@ -1,10 +1,9 @@
 package fr.esgi.rent.api;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 
-import java.io.IOException;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -16,43 +15,28 @@ public class RentalPropertyResource {
 
 
     @GET
-    public String getRentalProperties() {
-        return "coucou";
-        /*
-        String responseBody = "pas de reponse";
+    public Object getRentalProperties() {
         try {
             HttpClient client = HttpClient.newBuilder()
                     .connectTimeout(Duration.ofMinutes(1))
                     .build();
 
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("http://localhost:3000/hero"))
+                    .uri(URI.create("http://localhost:3000 /rent-properties-api/rental-properties"))
                     .GET()
                     .build();
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            int statusCode = response.statusCode();
-            responseBody = response.body();
-            System.out.println(responseBody);
-            if (statusCode >= 200 && statusCode < 300) {
-                return responseBody;
-            } else if (statusCode >= 400 && statusCode < 500) {
-                return ("Client error: " + responseBody);
-            } else if (statusCode >= 500 && statusCode < 600) {
-                return ("Server error: " + responseBody);
-            } else {
-                return ("Unknown status code: " + statusCode);
-            }
+            //Passer la response.body à un objet non typé permet de le parer
+            Object o = response.body();
+            return o;
         }
-        catch (IOException e){
-            return "IOException : " + e;
+        catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        catch (InterruptedException e){
-            return "InterruptedException : " + e;
-        }*/
-
     }
+
 
     /*@GET
     @Path("/{playerId}")
