2f7f08d9131b21d262599df3d481111395187e4d
Emmanuel <manuboisset@gmail.com>
2023-06-09 23:15:00 +0200

    add tests for post method

--- RentPropertiesAPI/src/test/java/fr/rent/application/RentPropertyControllerTest.java
+++ RentPropertiesAPI/src/test/java/fr/rent/application/RentPropertyControllerTest.java
@@ -2,6 +2,7 @@ package fr.rent.application;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.rent.domain.entity.RentPropertyEntity;
+import fr.rent.dto.RentPropertyRequestDto;
 import fr.rent.dto.RentPropertyResponseDto;
 import fr.rent.mapper.RentPropertyDtoMapper;
 import fr.rent.repository.RentPropertyRepository;
@@ -14,12 +15,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 import java.util.Optional;
 
-import static fr.rent.samples.RentPropertyDtoSample.oneRentalPropertyResponse;
-import static fr.rent.samples.RentPropertyDtoSample.rentalPropertyResponseList;
+import static fr.rent.samples.RentPropertyDtoSample.*;
 import static fr.rent.samples.RentPropertyEntitySample.oneRentalPropertyEntity;
 import static fr.rent.samples.RentPropertyEntitySample.rentalPropertyEntities;
 import static org.mockito.Mockito.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -88,5 +90,40 @@ class RentPropertyControllerTest {
         verifyNoInteractions(rentalPropertyDtoMapper);
         verifyNoMoreInteractions(rentalPropertyRepository);
     }
+
+    @Test
+    void shouldCreateRentalProperty() throws Exception {
+        RentPropertyRequestDto rentalPropertyRequestDto = oneRentalPropertyRequest();
+        RentPropertyResponseDto rentalPropertyResponseDto = oneRentalPropertyResponse();
+        RentPropertyEntity rentalPropertyEntity = oneRentalPropertyEntity();
+
+        when(rentalPropertyDtoMapper.mapToEntity(rentalPropertyRequestDto)).thenReturn(rentalPropertyEntity);
+        when(rentalPropertyRepository.save(rentalPropertyEntity)).thenReturn(rentalPropertyEntity);
+        when(rentalPropertyDtoMapper.mapToDto(rentalPropertyEntity)).thenReturn(rentalPropertyResponseDto);
+
+        mockMvc.perform(post("/rent-properties-api/rental-properties")
+                        .contentType(APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(rentalPropertyRequestDto)))
+                .andExpect(status().isCreated());
+
+        verify(rentalPropertyDtoMapper).mapToEntity(rentalPropertyRequestDto);
+        verify(rentalPropertyRepository).save(rentalPropertyEntity);
+        verifyNoMoreInteractions(rentalPropertyDtoMapper, rentalPropertyRepository);
+    }
+
+    @Test
+    void givenInvalidRequestBody_shouldReturn404HttpStatusCode() throws Exception {
+
+        RentPropertyRequestDto invalidRequest = oneRentalPropertyRequestWithInvalidValue();
+
+        mockMvc.perform(post("/rent-properties-api/rental-properties")
+                        .contentType(APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(invalidRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().json("{\"message\": \"La requête est incorrecte pour la raison suivante: L'un des champs renseignés est manquant ou incorrect\"}"));
+
+        verifyNoInteractions(rentalPropertyDtoMapper, rentalPropertyRepository);
+    }
+
 
 }
--- RentPropertiesAPI/src/test/java/fr/rent/application/error/ErrorHandlerTest.java
+++ RentPropertiesAPI/src/test/java/fr/rent/application/error/ErrorHandlerTest.java
@@ -2,6 +2,8 @@ package fr.rent.application.error;
 
 import fr.rent.exception.RentPropertyNotFoundException;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,6 +17,17 @@ class ErrorHandlerTest {
         ErrorHandler errorHandler = new ErrorHandler();
 
         ErrorDto errorDto = errorHandler.handleNotFoundRentalPropertyException(new RentPropertyNotFoundException(message));
+
+        assertThat(errorDto.message()).isEqualTo(message);
+    }
+
+    @Test
+    void shouldHandleMethodArgumentNotValidException() {
+        String message = "La requête est incorrecte pour la raison suivante: L'un des champs renseignés est manquant ou incorrect";
+
+        ErrorHandler errorHandler = new ErrorHandler();
+
+        ErrorDto errorDto = errorHandler.handleMethodArgumentNotValid();
 
         assertThat(errorDto.message()).isEqualTo(message);
     }
