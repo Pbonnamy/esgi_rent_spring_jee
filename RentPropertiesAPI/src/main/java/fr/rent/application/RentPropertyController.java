320482c70e6977e3db4d6eead20454955d036c02
Emmanuel <manuboisset@gmail.com>
2023-06-11 12:00:40 +0200

    pass to ResponseEntity

--- RentPropertiesAPI/src/main/java/fr/rent/application/RentPropertyController.java
+++ RentPropertiesAPI/src/main/java/fr/rent/application/RentPropertyController.java
@@ -6,6 +6,7 @@ import fr.rent.exception.RentPropertyNotFoundException;
 import fr.rent.mapper.RentPropertyDtoMapper;
 import fr.rent.repository.RentPropertyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,16 +25,17 @@ public class RentPropertyController {
 
 
     @GetMapping("/rental-properties")
-    public List<RentPropertyResponseDto> getRentalProperties() {
+    public ResponseEntity<List<RentPropertyResponseDto>> getRentalProperties() {
         List<RentPropertyEntity> rentalProperties = rentPropertyRepository.findAll();
 
-        return rentalPropertyDtoMapper.mapToDtoList(rentalProperties);
+        return ResponseEntity.ok().body(rentalPropertyDtoMapper.mapToDtoList(rentalProperties));
     }
 
     @GetMapping("/rental-properties/{id}")
-    public RentPropertyResponseDto getRentalPropertyById(@PathVariable int id) {
+    public ResponseEntity<RentPropertyResponseDto> getRentalPropertyById(@PathVariable int id) {
         return rentPropertyRepository.findById(id)
                 .map(rentalPropertyDtoMapper::mapToDto)
+                .map(ResponseEntity::ok)
                 .orElseThrow(() -> new RentPropertyNotFoundException(id));
     }
 
