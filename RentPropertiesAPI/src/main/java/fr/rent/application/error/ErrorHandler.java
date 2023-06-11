f0fbe3a59f33ed0b967e042731de62b563163eff
Emmanuel <manuboisset@gmail.com>
2023-06-11 18:28:25 +0200

    add handle message "not readable exception"

--- RentPropertiesAPI/src/main/java/fr/rent/application/error/ErrorHandler.java
+++ RentPropertiesAPI/src/main/java/fr/rent/application/error/ErrorHandler.java
@@ -3,6 +3,7 @@ package fr.rent.application.error;
 import fr.rent.exception.RentPropertyBadRequest;
 import fr.rent.exception.RentPropertyNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,6 +25,12 @@ public class ErrorHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ErrorDto handleMethodArgumentNotValid() {
         return new ErrorDto(new RentPropertyBadRequest("L'un des champs renseignés est manquant ou incorrect").getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ErrorDto handleHttpMessageNotReadableException() {
+        return new ErrorDto("La requête est mal formée ou un des champs est invalide");
     }
 
 
