86ef96f41407768e9b4a3e7304f1ae490e8311a2
noeoxycode <76428537+noeoxycode@users.noreply.github.com>
2023-06-16 21:51:16 +0200

    Constants in properties

--- RentFrontAPI/src/main/java/fr/esgi/api/Constants.java
+++ RentFrontAPI/src/main/java/fr/esgi/api/Constants.java
@@ -1,16 +1,45 @@
 package fr.esgi.api;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public final class Constants {
-    public static final String FRONT_API_URI = "front-api/";
-    public static final String RENTAL_PROPERTIES_URI = "rental-properties";
-    public static final String RENTAL_CARS_URI = "rental-cars";
-    public static final String PROPERTIES_URI_TARGET = "rent-properties-api";
-    public static final String CARS_URI_TARGET = "rent-cars-api";
-    public static final int SPRING_PORT = 3000;
-    public static final int FRONT_PORT = 8080;
-    public static final String HOST = "http://localhost:";
-    public static final String BASE_SPRING_URI = HOST + SPRING_PORT + "/";
-    public static final String BASE_FRONT_URI = HOST + FRONT_PORT + "/" + FRONT_API_URI;
+    public static final String FRONT_API_URI;
+    public static final String RENTAL_PROPERTIES_URI;
+    public static final String RENTAL_CARS_URI;
+    public static final String PROPERTIES_URI_TARGET;
+    public static final String CARS_URI_TARGET;
+    public static final int SPRING_PORT;
+    public static final int FRONT_PORT;
+    public static final String HOST;
+    public static final String BASE_SPRING_URI;
+    public static final String BASE_FRONT_URI;
+
+    static {
+        Properties config = new Properties();
+        try (InputStream input = Constants.class.getClassLoader().getResourceAsStream("config.properties")) {
+            config.load(input);
+        } catch (IOException e) {
+            try {
+                throw new IOException(e.getMessage(), e);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex.getMessage(), e);
+            }
+        }
+
+        FRONT_API_URI = config.getProperty("front.api.uri");
+        RENTAL_PROPERTIES_URI = config.getProperty("rental.properties.uri");
+        RENTAL_CARS_URI = config.getProperty("rental.cars.uri");
+        PROPERTIES_URI_TARGET = config.getProperty("properties.uri.target");
+        CARS_URI_TARGET = config.getProperty("cars.uri.target");
+        SPRING_PORT = Integer.parseInt(config.getProperty("spring.port"));
+        FRONT_PORT = Integer.parseInt(config.getProperty("front.port"));
+        HOST = config.getProperty("host");
+        BASE_SPRING_URI = HOST + SPRING_PORT + "/";
+        BASE_FRONT_URI = HOST + FRONT_PORT + "/" + FRONT_API_URI;
+    }
 
     private Constants() {
     }
---
+++ RentFrontAPI/src/test/java/fr/esgi/api/api/ConstantsTest.java
@@ -0,0 +1,43 @@
+package fr.esgi.api.api;
+
+import fr.esgi.api.Constants;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.util.Properties;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ConstantsTest {
+    
+    @Mock
+    Constants constants;
+
+    @Test
+    public void testConstantsInitialization() {
+        assertEquals("front-api/", Constants.FRONT_API_URI);
+        assertEquals("rental-properties", Constants.RENTAL_PROPERTIES_URI);
+        assertEquals("rental-cars", Constants.RENTAL_CARS_URI);
+        assertEquals("rent-properties-api", Constants.PROPERTIES_URI_TARGET);
+        assertEquals("rent-cars-api", Constants.CARS_URI_TARGET);
+        assertEquals(3000, Constants.SPRING_PORT);
+        assertEquals(8080, Constants.FRONT_PORT);
+        assertEquals("http://localhost:", Constants.HOST);
+        assertEquals("http://localhost:3000/", Constants.BASE_SPRING_URI);
+        assertEquals("http://localhost:8080/front-api/", Constants.BASE_FRONT_URI);
+    }
+
+}
