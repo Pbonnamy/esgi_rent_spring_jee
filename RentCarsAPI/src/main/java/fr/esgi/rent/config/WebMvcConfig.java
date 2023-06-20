f1bb28cceb78e5c84d33731b332571c7898553f2
Pierre <pbonnamy.pro@gmail.com>
2023-06-20 10:53:37 +0200

    handle controller tests

--- RentCarsAPI/src/main/java/fr/esgi/rent/config/WebMvcConfig.java
+++ RentCarsAPI/src/main/java/fr/esgi/rent/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package fr.esgi.rent.config;
 
 import fr.esgi.rent.interceptor.AuthInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,8 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Bean
+    public AuthInterceptor authInterceptor() {
+        return new AuthInterceptor();
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor());
+        registry.addInterceptor(authInterceptor());
     }
 }
